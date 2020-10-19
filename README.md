9d49c032e0cef6438ba56a2248f36f810ff9c851
lamartire <lamartire@gmail.com>
2020-10-19 15:02:28 +0300

    docs: update readme and add preview

--- README.md
+++ README.md
@@ -1,13 +1,29 @@
 # v-color
 
-Chrome-flavored color picker in Vue.js
+<img src="public/preview.png" />
 
 ## Install
 
+With `npm`: 
 ```
-yarn add v-color
-
-# or
-npm install --save v-color
+npm install --save @uscreen-video/v-color
 ```
 
+Or with `yarn`:
+
+```
+yarn add @uscreen-video/v-color
+```
+
+## Props
+
+|Name|Type|Default|Description|
+|-|-|-|-|
+| `value` | `string` | `''` | Color string in `hex`, `rgba` or `hsla` format |
+| `withSuggestions` | `boolean` | `true` | Enables colors suggestions which stores in local storage. Picker remembers 7 last colors |
+
+## Events
+
+|Name|Payload|Description|
+|-|-|-|
+|`input`|`string`|Emits on each color change|
--- demo.js
+++ demo.js
@@ -3,11 +3,63 @@ window.Vue.use(ColorPicker)
 
 window.vm = new window.Vue({
   el: '#app',
-  template: `<color-picker v-model="color">
-</color-picker>`,
+  template: `
+    <div style="display: flex;">
+      <div style="margin-right: 36px">
+        <color-picker v-model="hex" />
+        <div :style="{
+          width: '180px',
+          height: '40px',
+          'line-height': '40px',
+          'text-align': 'center',
+          margin: '20px auto 0',
+          color: 'white',
+          'background-color': hex,
+          'border-radius': '20px',
+          'font-family': 'Inter, Helvetica, Arial, sans-serif'
+        }">
+          {{ hex }}
+        </div>
+      </div>
+      <div style="margin-right: 36px">
+        <color-picker v-model="rgba" />
+        <div :style="{
+          width: '180px',
+          height: '40px',
+          'line-height': '40px',
+          'text-align': 'center',
+          margin: '20px auto 0',
+          color: 'white',
+          'background-color': rgba,
+          'border-radius': '20px',
+          'font-family': 'Inter, Helvetica, Arial, sans-serif'
+        }">
+          {{ rgba }}
+        </div>
+      </div>
+      <div>
+        <color-picker v-model="hsla" />
+        <div :style="{
+          width: '180px',
+          height: '40px',
+          'line-height': '40px',
+          'text-align': 'center',
+          margin: '20px auto 0',
+          color: 'white',
+          'background-color': hsla,
+          'border-radius': '20px',
+          'font-family': 'Inter, Helvetica, Arial, sans-serif'
+        }">
+          {{ hsla }}
+        </div>
+      </div>
+    </div>
+  `,
   data () {
     return {
-      color: '#0aa0ff'
+      hex: '#f00',
+      rgba: 'rgba(255,0,0,1)',
+      hsla: 'hsla(0,100%,50%,1)'
     }
   },
   watch: {
